e17efc3bb1ed049d41bd909ae745571d3f3903d2
Sharad Barad <sharadbarad111+github@gmail.com>
2024-03-17 16:58:49 +0530

    responsive

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,8 +1,54 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+
 // For menu bar in mobile 
-document.querySelector('.menu-icon').addEventListener('click', function() {
-    document.querySelector('.sidebar').style.display='block';
+$('.menu-icon').click(function() {
+    $('.sidebar').css('display', 'block');
 });
 
-document.querySelector('.close-icon').addEventListener('click', function() {
-    document.querySelector('.sidebar').style.display='none';
+$('.close-icon').click(function() {
+    $('.sidebar').css('display', 'none');
+});
+
+
+// Animations when reach at element 
+// For training centers
+var effectApplied = [];
+
+$('.training-center').waypoint(function(direction) {
+    var $element = $(this.element);
+    if(effectApplied.indexOf($element[0]) === -1){
+        $element.addClass('scale');
+        effectApplied.push($element[0]);
+
+        // Remove the class after a specific time
+        setTimeout(function() {
+            $element.removeClass('scale');
+        }, 200);
+    }
+
+}, {
+    offset: '45%'
+});
+
+// For pricing cards
+var effectApplied_price = [];
+
+$('.plan').waypoint(function(direction) {
+    var $element = $(this.element);
+    if(effectApplied_price.indexOf($element[0]) === -1){
+        $element.addClass('highlight');
+        effectApplied_price.push($element[0]);
+
+        // Remove the class after a specific time
+        setTimeout(function() {
+            $element.removeClass('highlight');
+        }, 300);
+    }
+
+}, {
+    offset: '40%'
 });
